6974a6e99882301ad3b09f3a77a6290027bc1956
Parker <pp101socials@gmail.com>
2026-04-12 09:06:45 -0400

    Edited file

--- assets/functions/Login_Register/SwitchToLogin.js
+++ assets/functions/Login_Register/SwitchToLogin.js
@@ -9,10 +9,12 @@ import ClearInputFields from "../Global/forms/ClearInputFields";
 export default function switchToLogin() {
         const LOGIN_FORM_HOLDER = document.getElementById("loginFormHolder");
         const REGISTER_FORM_HOLDER = document.getElementById("registerFormHolder");
+        const FORGOT_PASSWORD_FORM_HOLDER = document.getElementById("forgotPasswordFormHolder");
         const SWITCH_TO_REGISTER_BTN = document.getElementById("switchToRegister");
         const SWITCH_TO_LOGIN_BTN = document.getElementById("switchToLogin");
 
         LOGIN_FORM_HOLDER.style.display = 'block';
+        FORGOT_PASSWORD_FORM_HOLDER.style.display = 'none';
         REGISTER_FORM_HOLDER.style.display = 'none';
         SWITCH_TO_REGISTER_BTN.style.display = 'block'
         SWITCH_TO_LOGIN_BTN.style.display = 'none';
